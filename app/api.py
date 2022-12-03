dd292882e099896fe2c4301325b67ee6eb9f6670
Eric Arellano <ericarellano@github.com>
2022-12-03 00:22:01 -0400

    App built

---
+++ app/api.py
@@ -0,0 +1,33 @@
+import asyncio
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
+
+
+app = FastAPI()
+counter_lock = asyncio.Lock()
+counter = 0
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+# serve HTML template with per process counter
+@app.get("/", response_class=HTMLResponse)
+@app.get("/index.html", response_class=HTMLResponse)
+async def index(request: Request):
+    global counter
+    async with counter_lock:
+        counter += 1
+    return templates.TemplateResponse("index.html", {"request": request, "counter": counter})
+
+
+# serve static html file
+@app.get("/page1")
+async def read_index():
+    return FileResponse('static/page1.html')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
