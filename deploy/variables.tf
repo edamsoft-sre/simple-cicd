fef262a353685ca0dff7f8c7b2a370f93083e7c6
Eric Arellano <ericarellano@github.com>
2022-12-05 00:35:17 -0400

    terraform fmt

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,19 +1,19 @@
 variable "kube_config_context" {
-  type = string
+  type        = string
   description = "Name of Kube config context to deploy with from config at KUBE_CONFIG_PATH"
 }
 
 variable "deployment_yaml" {
-  type = string
+  type        = string
   description = "Kubernetes Deploy Manifest"
 }
 
 variable "image_name" {
-  type = string
+  type        = string
   description = "The image to be deployed"
 }
 
 variable "missing_var" {
-  type = string
+  type        = string
   description = "Used just for testing tfvars auto-updates"
 }
